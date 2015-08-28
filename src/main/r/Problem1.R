40df1f4ae1a0c7e0ca230e2db21aee0d817a911f
Kevin Lane <lane.kevin.a@gmail.com>
2015-08-28 12:47:24 -0700

    Fix tabs in Problem1.R.

--- src/main/r/Problem1.R
+++ src/main/r/Problem1.R
@@ -1,17 +1,17 @@
 solve <- function() {
 
-	limit <- 1000
-	multiples <- c(3, 5)
-	result <- 0
+    limit <- 1000
+    multiples <- c(3, 5)
+    result <- 0
 
-	for (i in min(multiples):limit-1) {
-		for (j in multiples) {
-		    if (i %% j == 0) {
-				result <- result + i
-				break
-			}
-		}
-	}
+    for (i in min(multiples):limit-1) {
+        for (j in multiples) {
+            if (i %% j == 0) {
+                result <- result + i
+                break
+            }
+        }
+    }
 
-	return(result)
+    return(result)
 }
