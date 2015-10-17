228cf94e54b392c4533165c4e405b06cbe8dba64
Kevin Lane <lane.kevin.a@gmail.com>
2015-10-17 08:43:41 -0700

    Use vector operations in R problem 1 to be more compact.

--- src/main/r/problems001_020/Problem1.R
+++ src/main/r/problems001_020/Problem1.R
@@ -1,17 +1,6 @@
 solve <- function() {
-
     limit <- 1000
     multiples <- c(3, 5)
-    result <- 0
-
-    for (i in min(multiples):limit-1) {
-        for (j in multiples) {
-            if (i %% j == 0) {
-                result <- result + i
-                break
-            }
-        }
-    }
-
-    return(result)
+    values <- rep(seq(min(multiples), limit-1), length(multiples))
+    return(sum(unique(values[(values %% sort(rep(multiples, length(values)/2))) == 0])))
 }
